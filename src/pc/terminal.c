deeaf01cae7dff8e2b1558e1d684ace166547e44
EmeraldLockdown <86802223+EmeraldLoc@users.noreply.github.com>
2026-05-04 11:00:17 -0500

    Allow terminal to use both `/` and no slash format

--- src/pc/terminal.c
+++ src/pc/terminal.c
@@ -83,7 +83,7 @@ void terminal_update() {
         terminal_stop();
 
         if (input[0] != '\0') {
-            run_command(input);
+            run_command(input[0] == '/' ? input + 1 : input);
             linenoiseHistoryAdd(input);
         }
 
