180a59815f9036a029c6fc916234c9206676ae79
EmeraldLockdown <86802223+EmeraldLoc@users.noreply.github.com>
2026-05-08 13:31:59 -0500

    idk what this is

--- src/pc/utils/misc.c
+++ src/pc/utils/misc.c
@@ -616,7 +616,7 @@ static char *get_update_path() {
 
 bool can_update_game() {
     // the file is not guaranteed to exist, so make sure we have the updater installed
-    return fs_sys_file_exists(get_update_path()) && gUpdateMessage;
+    return fs_sys_file_exists(get_update_path()) && !gUpdateMessage;
 }
 
 void update_game(void) {
