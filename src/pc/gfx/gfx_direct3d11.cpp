eebf32b2e5cd165187a0c41f1330bf2a38421cae
EmeraldLoc <calebelm@icloud.com>
2026-08-08 21:13:00 -0500

    Fix resizing in directx

--- src/pc/gfx/gfx_direct3d11.cpp
+++ src/pc/gfx/gfx_direct3d11.cpp
@@ -1150,10 +1150,10 @@ static void gfx_d3d11_start_frame(void) {
     struct FramePass *framePass = gfx_get_current_frame_pass();
 
     // get current render targets for clearing
-    ID3D11RenderTargetView *currentRtv = nullptr;
-    ID3D11DepthStencilView *currentDsv = nullptr;
+    ComPtr<ID3D11RenderTargetView> currentRtv;
+    ComPtr<ID3D11DepthStencilView> currentDsv;
 
-    d3d.context->OMGetRenderTargets(1, &currentRtv, &currentDsv);
+    d3d.context->OMGetRenderTargets(1, currentRtv.GetAddressOf(), currentDsv.GetAddressOf());
 
     // Prepare clear colors
     float clearColor[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
@@ -1165,8 +1165,8 @@ static void gfx_d3d11_start_frame(void) {
     }
 
     // clear render targets
-    d3d.context->ClearRenderTargetView(currentRtv, clearColor);
-    d3d.context->ClearDepthStencilView(currentDsv, D3D11_CLEAR_DEPTH, 1.0f, 0);
+    d3d.context->ClearRenderTargetView(currentRtv.Get(), clearColor);
+    d3d.context->ClearDepthStencilView(currentDsv.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0);
 
     frameCount++;
 }
